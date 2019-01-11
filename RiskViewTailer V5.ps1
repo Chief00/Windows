f0648315a1aa64b0973cc0094e68d40a4b1d71c4
Joshua Molloy <joshuajmolloy@gmail.com>
2019-01-11 16:07:18 +0000

    Cleaned up code, made stability better, sub choices

--- RiskViewTailer V5.ps1
+++ RiskViewTailer V5.ps1
@@ -1,139 +1,152 @@
 cd C:\Users\$([Environment]::UserName)\AppData\Local\Temp
 
 
+function printLogo {
+Write-Host "
+           ___  _     __  _   ___
+          / _ \(_)__ / /_| | / (_)__ _    __
+         / , _/ (_-</  '_/ |/ / / -_) |/|/ /
+        /_/|_/_/___/_/\_\|___/_/\__/|__,__/
+        /_  __/__ _(_) /__ ____
+         / / / _ '/ / / -_) __/
+        /_/  \_,_/_/_/\__/_/
+
+
+"
+}
+
+function choiceLoop ($Low, $High, $userFunction){
+    $choice = ""
+	while (($choice -lt $Low) -or ($choice -gt $High)){
+        $choice = $userFunction
+	}
+}
+
+function userChoicesList ($title, $choiceArray) {
+
+    Write-Host $title `n
+    for ($i = 0; $i -lt $choiceArray.length; $i++) {
+        Write-Host "[$i]" $choiceArray[$i]
+    }
+    $choice = Read-Host "`n`nWhat do you choose? "
+    return $choice
+}
+
 function mainChoice {
-
-Read-Host "
-
-       ___  _     __  _   ___           
-      / _ \(_)__ / /_| | / (_)__ _    __
-     / , _/ (_-</  '_/ |/ / / -_) |/|/ /
-    /_/|_/_/___/_/\_\|___/_/\__/|__,__/ 
-    /_  __/__ _(_) /__ ____             
-     / / / _ '/ / / -_) __/             
-    /_/  \_,_/_/_/\__/_/                
-                                    
-
- 
-Tail the log:
-[1] Since App Opened
-[2] Since Last Gather
-[3] Never
-[4] All
-[5] Search - Simple
-[6] Search - Extended
-[7] Free Ram 
-[8] Current File
-[9] Search - Tail
-
-What do you choose?"
-
+    cls
+    printLogo
+    $choice = userChoicesList "What do you want to tail: " $choiceArray
+    return $choice
 }
 
 function searchSubChoice {
-
-Read-Host "
-
-       ___  _     __  _   ___           
-      / _ \(_)__ / /_| | / (_)__ _    __
-     / , _/ (_-</  '_/ |/ / / -_) |/|/ /
-    /_/|_/_/___/_/\_\|___/_/\__/|__,__/ 
-    /_  __/__ _(_) /__ ____             
-     / / / _ '/ / / -_) __/             
-    /_/  \_,_/_/_/\__/_/                
-                                    
-
-
-Search
-[1] Simple
-[2]	Extended
-[3]	Tailing
- "
+    cls
+    printLogo
+    $choice = userChoicesList "What type of search: " $searchSubChoiceArray
+    return $choice
 }
 
 
 function logChoice ($choice){
 
-if ($choice -eq 1){
-get-content riskview-cs.log -Wait -Tail ((select-string riskview-cs.log -Pattern ":" | select-object -ExpandProperty 'LineNumber' -Last 1) - (select-string riskview-cs.log -Pattern "main info" | select-object -ExpandProperty 'LineNumber' -Last 1)+1)
+    if ($choice -eq "Since App Opened")  {
+        get-content riskview-cs.log -Wait -Tail ((select-string riskview-cs.log -Pattern ":" | select-object -ExpandProperty 'LineNumber' -Last 1) - (select-string riskview-cs.log -Pattern "main info" | select-object -ExpandProperty 'LineNumber' -Last 1)+1)
+    }
+
+    if ($choice -eq "Since Last Gather")  {
+        get-content riskview-cs.log -wait -Tail ((select-string riskview-cs.log -Pattern ":" | select-object -ExpandProperty 'LineNumber' -Last 1)-(select-string riskview-cs.log -Pattern "Requesting project details" | select-object -ExpandProperty 'LineNumber' -Last 1)+1)
+    }
+
+    if ($choice -eq "Never")   {
+        get-content riskview-cs.log -wait -Tail 0
+    }
+
+    if ($choice -eq "All"){
+        get-content riskview-cs.log -wait
+    }
+
+    if ($choice -eq "Search"){
+        $subChoice = ""
+        while (($subChoice -lt 0) -or ($subChoice -gt $searchSubChoiceArray.length)){
+            $subChoice = searchSubChoice
+            if (($subChoice -lt 0) -or ($subChoice -gt $searchSubChoiceArray.length)){
+                continue
+            }
+            $subChoice = logSearchChoice $searchSubChoiceArray[$subChoice]
+        }
+    }
+
+
+    if ($choice -eq "Ram Usage"){
+        get-content riskview-cs.log -wait -Tail ((select-string riskview-cs.log -Pattern ":" | select-object -ExpandProperty 'LineNumber' -Last 1)-(select-string riskview-cs.log -Pattern "Requesting project details" | select-object -ExpandProperty 'LineNumber' -Last 1)+1) | where {$_.contains("Current Free Memory")}
+    }
+
+    if ($choice -eq "Current File"){
+        get-content riskview-cs.log -wait -Tail ((select-string riskview-cs.log -Pattern ":" | select-object -ExpandProperty 'LineNumber' -Last 1)-(select-string riskview-cs.log -Pattern "Requesting project details" | select-object -ExpandProperty 'LineNumber' -Last 1)+1) | where {$_.contains("[FileGathererToItems] Processing file") -Or $_.contains("[TikaFileGathererBase] File Excluded")}
+    }
 }
 
-if ($choice -eq 2){
-get-content riskview-cs.log -wait -Tail ((select-string riskview-cs.log -Pattern ":" | select-object -ExpandProperty 'LineNumber' -Last 1)-(select-string riskview-cs.log -Pattern "Requesting project details" | select-object -ExpandProperty 'LineNumber' -Last 1)+1)
+function logSearchChoice ($subChoice) {
+
+    if ($subChoice -eq "Simple"){
+
+        $userString = Read-Host "What do you want to search? "
+        select-string riskview-cs.log -Pattern $userString -Context 0, 2
+
+        $ynLoop = Read-Host "Do you want to search something else? (y or n)"
+        if ($ynLoop -eq "y") {
+            $ynLoop = ""
+        }else {$ynLoop = 0}
+
+    }
+    if ($subChoice -eq "Extra"){
+
+            $userString = Read-Host "What do you want to search? "
+            $extraLines = Read-Host "How many extra lines do you want to print? "
+            select-string riskview-cs.log -Pattern $userString -Context 0, $extraLines
+
+            $ynLoop = Read-Host "Do you want to search something else? (y or n)"
+            if ($ynLoop -eq "y") {
+                $ynLoop = ""
+            }else {$ynLoop = 0}
+    }
+    if ($subChoice -eq "Tailing"){
+        $ynLoop = 0
+        ""
+        "This is case sensitive"
+        $userString = Read-Host "What do you want to search? "
+        get-content riskview-cs.log -wait -Tail ((select-string riskview-cs.log -Pattern ":" | select-object -ExpandProperty 'LineNumber' -Last 1)-(select-string riskview-cs.log -Pattern "Requesting project details" | select-object -ExpandProperty 'LineNumber' -Last 1)+1) | where {$_.contains($userString)}
+    }
+
+    return $ynLoop
 }
 
-if ($choice -eq 3 -or ""){
-get-content riskview-cs.log -wait -Tail 0
-} 
-
-if ($choice -eq 4){
-get-content riskview-cs.log -wait
+function choiceHelp {
+    cls
+    Write-Host "Hello"
+    Read-Host "Press Enter to continue "
 }
-
-if ($choice -eq 5){
-
-
-$ynLoop = "y"
-while ($ynLoop -eq "y") {
-$userString = Read-Host "What do you want to search? "
-select-string riskview-cs.log -Pattern $userString -Context 0, 2
-
-$ynLoop = Read-Host "Do you want to search something else? (y or n)"
-}
-}
-
-if ($choice -eq 6){
-$ynLoop = "y"
-while ($ynLoop -eq "y") {
-$userString = Read-Host "What do you want to search? "
-$extraLines = Read-Host "How many extra lines do you want to print? "
-select-string riskview-cs.log -Pattern $userString -Context 0, $extraLines
-
-$ynLoop = Read-Host "Do you want to search something else? (y or n)"
-}
-}
-
-if ($choice -eq 9){
-""
-"This is case sensitive"
-$userString = Read-Host "What do you want to search? "
-get-content riskview-cs.log -wait -Tail ((select-string riskview-cs.log -Pattern ":" | select-object -ExpandProperty 'LineNumber' -Last 1)-(select-string riskview-cs.log -Pattern "Requesting project details" | select-object -ExpandProperty 'LineNumber' -Last 1)+1) | where {$_.contains($userString)}
-}
-
-if ($choice -eq 7){
-get-content riskview-cs.log -wait -Tail ((select-string riskview-cs.log -Pattern ":" | select-object -ExpandProperty 'LineNumber' -Last 1)-(select-string riskview-cs.log -Pattern "Requesting project details" | select-object -ExpandProperty 'LineNumber' -Last 1)+1) | where {$_.contains("Current Free Memory")}
-}
-
-if ($choice -eq 8){
-get-content riskview-cs.log -wait -Tail ((select-string riskview-cs.log -Pattern ":" | select-object -ExpandProperty 'LineNumber' -Last 1)-(select-string riskview-cs.log -Pattern "Requesting project details" | select-object -ExpandProperty 'LineNumber' -Last 1)+1) | where {$_.contains("[FileGathererToItems] Processing file") -Or $_.contains("[TikaFileGathererBase] File Excluded")}
-}
-}
-
-
 
 # This is the user main input loop
-$userChoice = 0
-while (($userChoice -lt 1) -or ($userChoice -gt 9)){
-$userChoice = 0
-$userChoice = mainChoice
-cls
+$userChoice = ""
+$choiceArray = "Since App Opened", "Since Last Gather", "Never", "All", "Search", "Ram Usage", "Current File"
+$searchSubChoiceArray = "Simple", "Extra", "Tailing"
+while (($userChoice -lt 0) -or ($userChoice -gt $choiceArray.length-1)){
+    $userChoice = ""
+    $userChoice = $(mainChoice)
+    if ($userChoice -eq "?"){
+        choiceHelp
+    }elseif (($userChoice -lt 0) -or ($userChoice -gt $choiceArray.length-1)){
+        continue
+    }
+    logChoice $choiceArray[$userChoice]
 }
 
-function choiceLoop ($Low, $High,$choice){
-	while (($choice -lt $Low) -or ($choice -gt $High)){
-	$userChoice = 0
-	$userChoice = mainChoice
-	cls
-	}
-}
 
-
-logChoice $userChoice
 
 # Add cool graphics
-# Change search types to a sub choice instead of 2 main choices
 # Add choice to describe the choices
 # Look at other logs
-# Search Tailing
 # AND searching
 # Quit feature
+# Integers only, and yn only
