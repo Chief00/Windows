56b20b02816a19bbcb25a778629917f561d815ee
Joshua <joshuajmolloy@gmail.com>
2019-03-02 00:21:41 +0000

    added restart of tailor

--- RiskView/Updating.ps1
+++ RiskView/Updating.ps1
@@ -2,4 +2,5 @@
 rm RiskViewTailor.ps1
 $downloadURL = "https://raw.githubusercontent.com/Chief00/Windows/master/RiskView/RiskViewTailor.ps1"
 Invoke-WebRequest $downloadURL -OutFile RiskViewTailor.ps1
+Start-Process powershell.exe -ArgumentList "-File RiskViewTailor.ps1"
 Remove-Item -LiteralPath $MyInvocation.MyCommand.Path -Force
