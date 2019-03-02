ea1be2daa46f7c85a694bcc6806a66a8c6627e11
Chief00 <44926686+Chief00@users.noreply.github.com>
2019-03-02 00:07:45 +0000

    Update Updating.ps1

--- RiskView/Updating.ps1
+++ RiskView/Updating.ps1
@@ -1,1 +1,5 @@
 
+rm RiskViewTailor.ps1
+$downloadURL = "https://raw.githubusercontent.com/Chief00/Windows/master/RiskView/RiskViewTailor.ps1"
+Invoke-WebRequest $downloadURL -OutFile RiskViewTailor.ps1
+Remove-Item -LiteralPath $MyInvocation.MyCommand.Path -Force
